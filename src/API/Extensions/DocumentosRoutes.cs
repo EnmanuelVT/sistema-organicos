8c8accd08b1f107b385f7514d6d0918d48d14201
Emmanuel <evillavizartrinidad@gmail.com>
2025-08-29 18:12:36 -0400

    Empezar a trabajar con los documentos

--- src/API/Extensions/DocumentosRoutes.cs
+++ src/API/Extensions/DocumentosRoutes.cs
@@ -6,11 +6,12 @@ using Microsoft.AspNetCore.Builder;
 
 public static class DocumentosRoutes
 {
+    private static readonly string _assetsPath = Path.Combine(Directory.GetCurrentDirectory(), "Assets");
     public static RouteGroupBuilder MapDocumentosRoutes(this IEndpointRouteBuilder routes)
     {
         var group = routes.MapGroup("/api/documentos");
 
-        group.MapGet("/", async (DocumentoNegocio negocio) =>
+        group.MapGet("/formulario-agua-registro", async (DocumentoNegocio negocio) =>
         {
             //try
             //{
@@ -21,6 +22,23 @@ public static class DocumentosRoutes
             //{
             //    return Results.Problem(ex.Message);
             //}
+
+            try
+            {
+                var filePath = Path.Combine(_assetsPath, "FormularioAguaRegistro.xlsx");
+
+                if (!System.IO.File.Exists(filePath))
+                {
+                    return Results.NotFound("El archivo no fue encontrado.");
+                }
+
+                var fileBytes = await System.IO.File.ReadAllBytesAsync(filePath);
+                return Results.File(fileBytes, "application/vnd.openxmlformats-officedocument.spreadsheet");
+            }
+            catch (Exception ex)
+            {
+                return Results.Problem(ex.Message);
+            }
         });
 
         return group;
