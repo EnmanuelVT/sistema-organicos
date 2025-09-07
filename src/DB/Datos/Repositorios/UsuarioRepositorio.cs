4e5c03c384be79109a4b420e2cb4b290439e2723
Leandro <Leandropons2005@gmail.com>
2025-09-06 22:13:45 -0400

    Usuario/me

---
+++ src/DB/Datos/Repositorios/UsuarioRepositorio.cs
@@ -0,0 +1,52 @@
+﻿using ENTIDAD.DTOs.Users;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DB.Datos.Repositorios
+{
+    public class UsuarioRepositorio
+    {
+        private readonly MasterDbContext _context;
+
+        public UsuarioRepositorio(MasterDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<UserDto>> ObtenerUsuarioAsync(string usuarioId)
+        {
+            // Validate analyst ID if provided
+            if (!string.IsNullOrEmpty(usuarioId))
+            {
+                var usuarioExists = await _context.Users.AnyAsync(u => u.Id == usuarioId);
+                if (!usuarioExists)
+                {
+                    throw new Exception($"The specified analyst ID {usuarioId} does not exist.");
+                }
+            }
+
+            //var muestrasIds = _context.BitacoraMuestras
+            //    .Where(b => b.IdAnalista == usuarioId)
+            //    .Select(b => b.IdMuestra)
+            //    .Distinct()
+            //    .ToList();
+
+            return await _context.Usuarios
+                .Where(m => m.Id == usuarioId)
+                .Select(m => new UserDto
+                {
+                    Id = m.Id,
+                    UserName = m.UserName,
+                    Email = m.Email,
+                    Nombre = m.Nombre,
+                    Apellido = m.Apellido
+                })
+                .ToListAsync();
+        }
+
+    }
+}
