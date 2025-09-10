f27b31c97166dfb5d341af300d48f1dab784b24d
Emmanuel <evillavizartrinidad@gmail.com>
2025-09-10 09:13:15 -0400

    Agregar Parametro Seeder para tener los parametros hechos automaticamente

--- src/API/Seed/AppDataSeeder.cs
+++ src/API/Seed/AppDataSeeder.cs
@@ -49,6 +49,51 @@ public static class AppDataSeeder
             );
         }
 
+        if (!context.ParametroNormas.Any())
+        {
+            // Agua
+            // ph, Sólidos Totales, Dureza
+            // Recuento de Microorganismos Aerobios Mesófilos
+            // Recuento Coliformes
+            // Coliformes Totales
+            
+            // Alimento
+            // Recuento de Microorganismos Aerobios Mesófilos
+            // Recuento Coliformes
+            // Coliformes Totales
+            // E. coli
+            // Salmonella spp.
+            // Estafilococos aureus
+            // Hongos
+            // Levaduras
+            // Esterilidad Comercial
+            // Listeria monocytogenes
+
+            context.ParametroNormas.AddRange(
+                // Agua
+                new ParametroNorma { NombreParametro = "ph", Unidad = "litros", TpmstId = 1, ValorMin = 4, ValorMax = 10 },
+                new ParametroNorma { NombreParametro = "Sólidos Totales", Unidad = "mg/L", TpmstId = 1, ValorMin = 500, ValorMax = 1500 },
+                new ParametroNorma { NombreParametro = "Dureza", Unidad = "mg/L", TpmstId = 1, ValorMin = 100, ValorMax = 300 },
+                new ParametroNorma { NombreParametro = "Recuento de Microorganismos", Unidad = "UFC/mL", TpmstId = 1, ValorMin = 0, ValorMax = 500 },
+                new ParametroNorma { NombreParametro = "Coliformes Totales", Unidad = "NMP/100mL", TpmstId = 1, ValorMin = 0, ValorMax = 10 },
+                
+                // Alimento
+                new ParametroNorma { NombreParametro = "Recuento de Microorganismos", Unidad = "UFC/g", TpmstId = 2, ValorMin = 0, ValorMax = 100000 },
+                new ParametroNorma { NombreParametro = "Coliformes Totales", Unidad = "NMP/g", TpmstId = 2, ValorMin = 0, ValorMax = 100 },
+                new ParametroNorma { NombreParametro = "E. coli", Unidad = "NMP/g", TpmstId = 2, ValorMin = 0, ValorMax = 10 },
+                new ParametroNorma { NombreParametro = "Salmonella spp.", Unidad = "Presencia/25g", TpmstId = 2, ValorMin = 0, ValorMax = 0 },
+                new ParametroNorma { NombreParametro = "Estafilococos aureus", Unidad = "UFC/g", TpmstId = 2, ValorMin = 0, ValorMax = 100 },
+                new ParametroNorma { NombreParametro = "Hongos", Unidad = "UFC/g", TpmstId = 2, ValorMin = 0, ValorMax = 1000 },
+                new ParametroNorma { NombreParametro = "Levaduras", Unidad = "UFC/g", TpmstId = 2, ValorMin = 0, ValorMax = 1000 },
+                new ParametroNorma { NombreParametro = "Esterilidad Comercial", Unidad = "Presencia/25g", TpmstId = 2, ValorMin = 0, ValorMax = 0 },
+                new ParametroNorma { NombreParametro = "Listeria monocytogenes", Unidad = "Presencia/25g", TpmstId = 2, ValorMin = 0, ValorMax = 0 },
+                
+                // Bebida alcoholica
+                // Grado Alcohólico
+                new ParametroNorma { NombreParametro = "Grado Alcohólico", Unidad = "% vol", TpmstId = 3, ValorMin = 5, ValorMax = 40 }
+            );
+        }
+
         await context.SaveChangesAsync();
     }
 }
